66615064a83452a69774dc56ca8eaa7d1fbe6d21
nikit34 <permikov134@mail.ru>
2021-05-06 15:30:40 +0300

    custom ptr

--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 
+class A{
+    public:
+        A(){
+            std::cout << "A()" << std::endl;
+        }
+        ~A(){
+            std::cout << "~A()" << std::endl;
+        }
+};
+
 template<typename T>
 class UniquePtr {
     public:
@@ -10,11 +20,37 @@ class UniquePtr {
         UniquePtr(T* ptr){
             this->ptr = ptr;
         }
+        UniquePtr(UniquePtr&& ref){
+            this->ptr = ref.ptr;
+            ref.ptr = nullptr;
+        }
+        UniquePtr& operator=(UniquePtr&& ref){
+            this->ptr = ref.ptr;
+            ref.ptr = nullptr;
+            return *this;
+        }
+
+        ~UniquePtr(){
+            delete this->ptr;
+        }
+
+        // prohibits constructor of copy
+        UniquePtr(const UniquePtr&) = delete;
+        // throught equally
+        UniquePtr& operator=(const UniquePtr&) = delete;
 
     private:
         T* ptr;
 };
 
 int main() {
+    {
+        UniquePtr<A> a_ptr(new A);
+        decltype(a_ptr) b_ptr = std::move(a_ptr);
+
+        // this do prohibitions on constructors
+        // decltype(a_ptr) b_ptr(a_ptr);
+        // decltype(a_ptr) b_ptr = a_ptr;
+    }
     return 0;
 }
